331bad04b0e43c47f005e2d567b9341d63a31bfb
Duke Nukem <joncherwin@gmail.com>
2023-03-16 21:12:43 -0500

    Added UPDATE and DELETE routes to item-routes, creating user cart on user signup, moved categories out of api folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,24 +4,29 @@ const { User } = require('../../models');
 // CREATE new user
 // eslint-disable-next-line no-use-before-define
 router.post('/', async (req, res) => {
-  try {
-    const dbUserData = await User.create({
-      name: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    try {
+        const dbUserData = await User.create({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.userId = dbUserData.id;
-      req.session.username = dbUserData.name;
+        const cartData = await Cart.create({
+            user_id: dbUserData.id
+        });
 
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.name;
+            req.session.cartId = cartData.id;
+
+            res.status(200).json(dbUserData);
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 // Login
