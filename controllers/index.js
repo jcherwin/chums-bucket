9c587bcf24060bb7d7492e9fdee5509694e4b20c
Jullian0426 <jullian0426@yahoo.com>
2023-03-17 20:17:30 -0500

    Create header, navbar, and footer using mdb

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const categoryRoutes = require('./category-routes.js');
-//const cartRoutes = require('./cart-routes');
+const cartRoutes = require('./cart-routes');
 
 router.use('/', homeRoutes);
 router.use('/category', categoryRoutes);
-//router.use('/cart', cartRoutes);
+router.use('/cart', cartRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
