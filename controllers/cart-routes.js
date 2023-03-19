43143babe4fec14af658521f2f3407e8fe554df8
Jullian0426 <jullian0426@yahoo.com>
2023-03-18 23:01:06 -0500

    Fix eslint errors.

--- controllers/cart-routes.js
+++ controllers/cart-routes.js
@@ -5,27 +5,24 @@ const withAuth = require('../utils/auth');
 
 // Render the cart and a corresponding subtotal
 router.get('/', withAuth, async (req, res) => {
-    try
-    {
+    try {
         const cartData = await Cart.findOne({
             where: { user_id: req.session.userId },
             include: [ { model: Item } ]
         });
-      
+
         const getCart = (cart) => cart.get({ plain: true });
         const cart = getCart(cartData);
 
         // Use lodash to deconstruct price from the items and get a sum.
         const prices = _.map(cart.items, 'price');
-        cart.subtotal = _.sum(prices)
+        cart.subtotal = _.sum(prices);
 
         res.render('cart', {
             cart,
             loggedIn: req.session.loggedIn
         });
-    }
-    catch (err)
-    {
+    } catch (err) {
         res.status(500).json(err);
     }
 });
--- seeds/index.js
+++ seeds/index.js
@@ -5,15 +5,15 @@ const { Category, Product } = require('../models');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await Product.drop();
-  await Category.drop();
-  await sequelize.sync({ force: false });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+    await Product.drop();
+    await Category.drop();
+    await sequelize.sync({ force: false });
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedCategories();
+    console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+    await seedProducts();
+    console.log('\n----- PRODUCTS SEEDED -----\n');
 };
 
 module.exports = seedAll;
